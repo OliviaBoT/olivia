7d2356cbc000204dce6736ec2dfbf045ed712815
kartavyaxdd <86247494+kartavyaxdd@users.noreply.github.com>
2021-09-24 10:58:49 +0530

    update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -92,8 +92,8 @@ async def get_start_func(message, strings, edit=False):
     )
     buttons.add(
         InlineKeyboardButton(
-            "👸🏼 Add DaisyX to your group",
-            url=f"https://telegram.me/daisyxbot?startgroup=true",
+            "👸🏼 Add Olivia to your group",
+            url=f"https://telegram.me/Miss_OliviboT?startgroup=true",
         )
     )
     # Handle error when user click the button 2 or more times simultaneously
